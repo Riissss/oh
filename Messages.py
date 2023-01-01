e4d1c9e2b6379c17a968996ed2463ea765bc5a9a
Riissss <121730144+Riissss@users.noreply.github.com>
2023-01-01 20:37:13 +0700

    Add files via upload

---
+++ Messages.py
@@ -0,0 +1,42 @@
+m_start = ' Hai!\Anda masuk ke obrolan random chat!\n' \
+               'Tekan tombol di bawah ini untuk mulai berkomunikasi dengan lawan bicara. ' \
+               'Jika Anda ingin mengenal seseorang lebih baik, tekan tombol Suka dan dengan simpati timbal balik ' \
+               'Anda akan dapat mengetahui Nama panggilan pasangan. Jika Anda tidak menyukai lawan bicara,' \
+               'maka jangan ragu untuk mengklik tombol tidak suka dan memulai obrolan baru.\n' \
+               'Perhatian! Agar bot berhasil bekerja, Anda harus memiliki username, ' \
+               'periksa pengaturan dan pastikan Anda memilikinya terdaftar!\n' \
+               'Untuk mematikan bot, panggil perintah / stop. ' \
+               'Harap dicatat bahwa semua data anda akan dihapus, jika tidak, Anda akan tetap berada di database.'
+
+M_is_not_free_users = ' maaf, tapi tidak ada pengguna gratis saat ini. ' \
+                   'Segera setelah pengguna lain masuk, kami akan menghubungkan Anda!'
+m_is_connect = 'Pasangan telah ditemukan!\nKatakan, Hai untuk membalas pasangan anda 😊\n/next - Untuk mencari obrolan baru\n /stop - Untuk berhenti dari obrolan\n🔊 PROMOTE 🔊\nYuk masuk group ini: 👉 @anony_cari_pacar'
+
+m_play_again = ' apakah anda ingin berbicara dengan orang lain?'
+
+m_is_not_user_name = 'maaf, tetapi di bot kami hanya mungkin untuk berkomunikasi jika Anda memiliki username😓'
+
+m_good_bye = ' Bot: selamat tinggal, kami akan senang melihat Anda lagi!'
+
+m_disconnect_user = 'Lawan bicara telah meninggalkan percakapan!\nApakah Anda Ingin mengobrol dengan orang lain?\nKetik /search untuk mencari obrolan baru'
+
+M_failed = 'Bot: beberapa kesalahan telah terjadi!'
+
+m_like = ' Bot: Pilihan bagus!'
+
+m_dislike_user = 'Bot: Dialog telah berakhir'
+
+m_dislike_user_to = 'Bot: lawan bicara tidak menyukai Anda, kami mohon maaf'
+
+m_send_some_messages = 'Bot: Anda tidak dapat meneruskan pesan Anda sendiri'
+
+m_has_not_dialog = 'Anda bukan bagian dari dialog'
+
+dislike_str = '\ U0001F44E Dislike'
+
+like_str = '\ U0001f44d seperti'
+
+
+def m_all_like (x):
+    kembalikan 'lawan bicara menyukai Anda \ n' + 'loginnya:' + str (x) + \
+           '\Good luck untuk Anda dalam komunikasi Anda!Terima kasih telah bersama kami!'
---
+++ ohmyriiss.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "5809941857:AAEyrnyV59fxkf3w4m94Js7mrRH6LaBOv-s") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "ohmyriiss") # jangan dirubah agar tidak error
+GROUP = os.environ.get("GROUP", "anony_caripacar") # 
+CHANNEL = os.environ.get("CHANNEL", "anony_chanel") # 
